3d531a988fe30c07dac12f42c8ec79d8dd7817c7
pakavaliauskas <55452582+pakavaliauskas@users.noreply.github.com>
2019-10-19 17:26:11 +0300

    Update Testiniai_duomenys.py

--- Testiniai_duomenys.py
+++ Testiniai_duomenys.py
@@ -6,23 +6,28 @@ import numpy as np
 from open3d import *
 
 data = [
+    (0,1,0),
+    (0,1,4),
+    (6,1,0),
+    (6,1,4),
     (2,2,2),
-    (0,2,4),
-    (3,2,5),
-    (2,5,7),
-    (0,7,1),
-    (0,5,2),
-    (0,8,-1),
-]  
+    (0,1.5,1),
+    (0,0,3),
+    (0,-1,-1),
+    (3,1.5,0),
+    (3,1.5,4),
+] 
 df = pd.DataFrame(data)
 df.columns = 'x y z'.split()
 df
 
+df.to_csv('duomenys.csv')
+
 df.y.value_counts()
 
-df[df.y == 2]
+df[df.y == 1]
 
-pjuvis = df[df.y == 2]
+pjuvis = df[df.y == 1]
 pjuvis
 
 pcd = o3d.geometry.PointCloud()
@@ -35,18 +40,22 @@ colors = [
     (1,0,0),
     (1,0,0),
     (1,0,0),
-    (0,0,0),
-    (0,1,0),
-    (0,1,0),
-    (0,0,0),
+    (1,0,0),
+    (1,0,0),
+    (1,0,0),
+    (1,0,0),
+    (1,0,0),
+    (1,0,0),
+    (1,0,0),   
 ]
+
 pcd = o3d.geometry.PointCloud()
 pcd.points = o3d.utility.Vector3dVector(pjuvis.values)
 # pcd.points = o3d.utility.Vector3dVector(df.values)
 pcd.colors = o3d.utility.Vector3dVector(colors)
 o3d.visualization.draw_geometries([pcd])
 # taskai sujungiami linijomis
-lines = [[2, 0], [0, 3], [2, 3]]
+lines = [[0, 1], [0, 2], [1, 3], [2, 3]]
 colors = [[1, 0, 0] for i in range(len(lines))]
 line_set = LineSet()
 line_set.points = Vector3dVector(pcd.points)
